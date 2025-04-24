9ff44e7f27330f6605bcb23e00e1c545b8f8c14c
Glebn5 <glebnowikoff@yandex.ru>
2025-04-24 15:10:14 +0300

    Add dist/

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/play_widget/',
+  base: '/ai_widget/',
   build: {
     outDir: 'dist',      
     minify: false,        
